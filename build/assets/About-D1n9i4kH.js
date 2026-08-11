07b73d8b5405b0a203dda80b9c6b3d612a477664
txiaoqt <1natada.christopher.gutierrez@gmail.com>
2026-08-12 05:24:56 +0800

    build: update production build bundles and gitignore

--- build/assets/About-D1n9i4kH.js
+++ build/assets/About-D1n9i4kH.js
@@ -1,4 +1,4 @@
-import{n as l,u as g,j as e,A as f,N as h,L as a,o as y,p,G as j,c as w,F as v,U as o,i as r,q as x,v as N,C as c,B as C,k,w as O,x as d,y as P}from"./index-BuMe0cV-.js";/**
+import{o as l,u as g,j as e,A as f,N as h,L as a,p as y,q as p,G as j,c as w,F as v,U as o,k as r,v as x,w as N,C as c,B as C,l as k,x as O,y as d,z as P}from"./index-cyTFZJTt.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- build/assets/facebook-qW12ckAy.js
+++ build/assets/facebook-qW12ckAy.js
@@ -1,4 +1,4 @@
-import{n as o}from"./index-BuMe0cV-.js";/**
+import{o}from"./index-cyTFZJTt.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
